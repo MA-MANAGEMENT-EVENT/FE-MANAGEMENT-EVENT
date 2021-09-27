9d52a24b3295898d9f34a7cc0eb196c95e19b3d9
Risa Jessica Sitohang <risasitohang4@gmail.com>
2021-09-27 22:34:43 +0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,63 @@
 import Template from "../templates/Template";
+import Button from '@mui/material/Button';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
 const Login = () => {
+  const useStyles = makeStyles((theme) => ({
+    form: {
+      width: '100%',
+      marginTop:'10px',
+    },
+  }));
+
   return (
-    <>
-      <h1>login</h1>
-    </>
+    
+    <Container component="main" maxWidth="xs">
+    <Typography component="h1" variant="h5">
+         Log in
+        </Typography>
+    <form>
+    <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+     <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+      <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >Log in</Button>
+      <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+    </form>
+    
+    </Container>
   );
 };
 export default Login;
