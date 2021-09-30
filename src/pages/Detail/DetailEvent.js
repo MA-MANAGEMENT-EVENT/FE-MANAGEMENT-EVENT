a7817ac45d89cc4263ab1012351472ee22f13a1a
Andrean Setiawan <andrean.535180002@stu.untar.ac.id>
2021-09-30 13:27:40 +0700

    Update DetailEvent.js

--- src/pages/Detail/DetailEvent.js
+++ src/pages/Detail/DetailEvent.js
@@ -57,8 +57,16 @@ const mainFeaturedPost = {
 
 // const posts = [];
 const posts = {
-  description: `Anda belum dapat mendaftar event ini. 
-  Yuk buat akun sekarang agar bisa mendaftar di event i`
+  description: `There are many variations of passages of Lorem Ipsum available, 
+  but the majority have suffered alteration in some form, by injected humour, 
+  or randomised words which don't look even slightly believable. If you are going 
+  to use a passage of Lorem Ipsum, you need to be sure there isn't anything 
+  embarrassing hidden in the middle of text. All the Lorem Ipsum generators on 
+  the Internet tend to repeat predefined chunks as necessary, making this the first 
+  true generator on the Internet. It uses a dictionary of over 200 Latin words, 
+  combined with a handful of model sentence structures, to generate Lorem Ipsum 
+  which looks reasonable. The generated Lorem Ipsum is therefore always free from 
+  repetition, injected humour, or non-characteristic words etc.`,
 };
 
 const sidebar = {
