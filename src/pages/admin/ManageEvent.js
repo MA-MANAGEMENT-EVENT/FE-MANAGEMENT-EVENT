f4b41125470b25fab1f48907ef57d689f37866ba
Ong William Raven Wijaya <58552967+ravenwijaya@users.noreply.github.com>
2021-10-05 19:36:09 +0700

    add molecules cardevent admin

--- src/pages/admin/ManageEvent.js
+++ src/pages/admin/ManageEvent.js
@@ -1,14 +1,9 @@
 import React from "react";
-import Button from "../../atoms/button/Button";
 import TextField from "../../atoms/textfield/TextField";
 import Typography from "../../atoms/typography/Typhography";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "../../atoms/grid/index";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { Link } from "react-router-dom";
 import CardEvent from "../../molecules/cardevent";
 
 const useStyles = makeStyles((theme) => ({
@@ -73,14 +68,30 @@ const ManageEvent = () => {
         <Grid container spacing={4}>
           {cards.map((card) => (
             <CardEvent
-            card={card}
-            imageUrl={"https://source.unsplash.com/random"}
-            title={"Title"}
-            description={" This is a media card. You can use this section to describe the content."}
-            link={["/manageparticipant","/managespeaker","/managefeedback","/detailevent","/editevent","/delete"]}
-            linkText={["Participant","Speaker","Feedback","View","Edit","Delete"]}
+              card={card}
+              imageUrl={"https://source.unsplash.com/random"}
+              title={"Title"}
+              description={
+                " This is a media card. You can use this section to describe the content."
+              }
+              link={[
+                "/manageparticipant",
+                "/managespeaker",
+                "/managefeedback",
+                "/detailevent",
+                "/editevent",
+                "/delete",
+              ]}
+              linkText={[
+                "Participant",
+                "Speaker",
+                "Feedback",
+                "View",
+                "Edit",
+                "Delete",
+              ]}
             />
-            ))}
+          ))}
         </Grid>
       </Container>
     </>
